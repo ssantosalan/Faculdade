ef10833afc562d5d8652029e7b5055e5592e43de
Alan Santos <alanxdsi@gmail.com>
2021-03-12 20:16:37 -0300

    string to int

--- PI 1_ 2101_PROJETO INTEGRADOR DESENVOLVIMENTO DE LÓGICA/ADO_4/EX_1.java
+++ PI 1_ 2101_PROJETO INTEGRADOR DESENVOLVIMENTO DE LÓGICA/ADO_4/EX_1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class EX_1 {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		String operacaoStr;
+		int a;
 
 		System.out.println("1. Domingo");
 		System.out.println("2. Segunda-feira");
@@ -15,28 +15,28 @@ public class EX_1 {
 
 		System.out.print("Escrevar um valor de 1 a 7: ");
 
-		operacaoStr = entrada.next();
+		a = entrada.nextInt();
 
-		switch (operacaoStr) {
-		case "1":
+		switch (a) {
+		case 1:
 			System.out.println("1 corresponde a Domingo");
 			break;
-		case "2":
+		case 2:
 			System.out.println("2 corresponde a Segunda-feira!");
 			break;
-		case "3":
+		case 3:
 			System.out.println("3 corresponde a Terça-feira!");
 			break;
-		case "4":
+		case 4:
 			System.out.println("4 corresponde a Quarta-feira!");
 			break;
-		case "5":
+		case 5:
 			System.out.println("5 corresponde a Quinta-feira!");
 			break;
-		case "6":
+		case 6:
 			System.out.println("6 corresponde a Sexta-feira!");
 			break;
-		case "7":
+		case 7:
 			System.out.println("7 corresponde a Sábado!");
 			break;
 		default:
