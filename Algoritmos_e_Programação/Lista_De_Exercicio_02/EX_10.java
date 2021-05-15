f8755d0af5c02058f627caa6dd45365b95e85a77
Alan Santos <ss.alan@outlook.com>
2021-05-14 22:21:16 -0300

    Create EX_10.java

---
+++ Algoritmos_e_Programação/Lista_De_Exercicio_02/EX_10.java
@@ -0,0 +1,17 @@
+
+public class EX_10 {
+
+	public static void main(String[] args) {
+
+		double i = 1.0;
+		double soma = 0;
+
+		for (i = 1; i < 20; i++) {
+
+			soma = soma + (1 / i);
+		}
+
+		System.out.println(soma);
+
+	}
+}
