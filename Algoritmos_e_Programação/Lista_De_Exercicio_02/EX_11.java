1c4f5573f6a08534b1c505174010d0fe9d0bccad
Alan Santos <ss.alan@outlook.com>
2021-05-15 14:02:00 -0300

    Update EX_11.java

--- Algoritmos_e_Programação/Lista_De_Exercicio_02/EX_11.java
+++ Algoritmos_e_Programação/Lista_De_Exercicio_02/EX_11.java
@@ -6,23 +6,24 @@ public class EX_11 {
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Informe um número natural: ");
 		int N = entrada.nextInt();
-		
+
 		double i = 1.0;
 		double soma = 0;
 
-		for (i = 1; i < N; i++) {
-			
-			soma = soma + i;
-			
-			soma = soma - (1 / i);
-			i++;
-			soma = soma + (1 / i);
-			
+		while (i <= N) {
+
+			if (i % 2 == 0) {
+				soma = soma - (1 / i);
+				i++;
+			} else if (i % 2 != 0) {
+				soma = soma + (1 / i);
+				i++;
+			}
+
 		}
 
 		System.out.println(soma);
-		
-		
+
 		entrada.close();
 
 	}
