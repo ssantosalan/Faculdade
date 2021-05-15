dd01dcabfb43551dac3385c23633b876d8068b35
Alan Santos <ss.alan@outlook.com>
2021-05-14 21:40:51 -0300

    Update EX_07.java

--- Algoritmos_e_Programação/Lista_De_Exercicio_02/EX_07.java
+++ Algoritmos_e_Programação/Lista_De_Exercicio_02/EX_07.java
@@ -2,7 +2,17 @@
 public class EX_07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int i = 7;
+		while (i < 200) {
+
+			if (i % 7 == 0) {
+				System.out.println(i);
+
+			}
+
+			i++;
+		}
 
 	}
 
