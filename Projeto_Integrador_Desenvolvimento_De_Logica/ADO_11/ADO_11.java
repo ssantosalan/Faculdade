8c1d4cba7d1305a55f3ba1daa8256e6d81bada5e
Alan Santos <ss.alan@outlook.com>
2021-05-17 10:26:10 -0300

    Finalização da ADO 11

--- Projeto_Integrador_Desenvolvimento_De_Logica/ADO_11/ADO_11.java
+++ Projeto_Integrador_Desenvolvimento_De_Logica/ADO_11/ADO_11.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class ADO_11 {
 
-	// método que embalha os elementos de um vetor de inteiros
 	public static void embaralhar(int vetor[]) {
 
 		Random random = new Random();
@@ -14,85 +12,136 @@ public class ADO_11 {
 			// sorteia um índice
 			int j = random.nextInt(vetor.length);
 
-			// troc ao conteúdo dos índices i e j do vetor
+			// troca o conteúdo dos índices i e j do vetor
 			int temp = vetor[i];
 			vetor[i] = vetor[j];
 			vetor[j] = temp;
 
 		}
-
 	}
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		String bomba = "b";
-		String espaco_marcado = "x";
-		String espaco_livre = "_";
 
-		System.out.print("Digite o tamanho do caminho: ");
-		int n = entrada.nextInt();
+		int vida = 1;
+		int escolha;
+		int pontuacao = 0;
+		int n;
+		
+		do {
+			System.out.print("Digite o tamanho do caminho: ");
+			n = entrada.nextInt();
+		} while (n <= 0);
 		int vetor[] = new int[n];
-		int contador = n;
-		
-		System.out.print("Digite o número de bombas: ");
-		int quantidade_bomba = entrada.nextInt();
-		System.out.println();
+		int quantidade_bomba;
 
-		
+		do {
+			System.out.print("Digite o número de bombas: ");
+			quantidade_bomba = entrada.nextInt();
+		} while (quantidade_bomba <= 0);
+
+		int pontuacaoMaxima = n - quantidade_bomba;
+
 		for (int i = 0; i < vetor.length; i++) {
 
-			// System.out.print("[" + i + "]");
-			
-			
 			if (quantidade_bomba > 0) {
 				vetor[i] = -1;
 				quantidade_bomba--;
 			} else {
 				vetor[i] = 0;
 			}
+
 		}
 
 		System.out.println();
 		// embaralha o array
 		embaralhar(vetor);
-		
-		
-		
-		
-		for (int i = 0; i < vetor.length; i++) {
-			
-			if (vetor[i] == -1) 
-			
-			/*
-			do { 
-				System.out.print(" _ ");
-				contador--;
-			} while (contador > 0);
-			*/
-			System.out.println();
-			for (i = 0; i < vetor.length; i++) {
 
+		while (vida > 0) {
+			for (int item : vetor) {
+
+				if (item == 0 || item == -1) {
+					System.out.print(" _ ");
+				} else if (item == 1) {
+					System.out.print(" x ");
+				}
+			}
+
+			System.out.println();
+			for (int i = 0; i < vetor.length; i++) {
 				System.out.print("[" + i + "]");
 
 			}
-			System.out.println();
-			System.out.print("Informe uma posição: ");
 
+			do {
+				System.out.println();
+				System.out.print("Informe o caminho: ");
+				escolha = entrada.nextInt();
+
+				if (vetor[escolha] == -1) {
+					System.out.println();
+					for (int item : vetor) {
+
+						if (item == 0) {
+							System.out.print(" _ ");
+						} else if (item == 1) {
+							System.out.print(" x ");
+						} else if (item == -1) {
+							System.out.print(" b ");
+						}
+					}
+					System.out.println();
+					for (int i = 0; i < vetor.length; i++) {
+						System.out.print("[" + i + "]");
+					}
+					System.out.println();
+					System.out.println("Fim de jogo!");
+					System.out.println("Pontuação: " + pontuacao + "/" + pontuacaoMaxima);
+					vida--;
+					System.exit(0);
+				} else if (vetor[escolha] == 1) {
+					System.out.println("Caminho já escolhido!");
+				}
+
+			} while (vetor[escolha] == 1);
+
+			if (vetor[escolha] == 0) {
+				pontuacao++;
+
+				if (escolha != 0 && escolha != n - 1) {
+					if (vetor[escolha + 1] == -1 || vetor[escolha - 1] == -1) {
+						System.out.println("Cuidado: bomba próxima!");
+					}
+				}
+
+			}
+
+			if (pontuacao == pontuacaoMaxima) {
+				System.out.println();
+				vetor[escolha] = 1;
+				for (int item : vetor) {
+
+					if (item == 0) {
+						System.out.print(" _ ");
+					} else if (item == 1) {
+						System.out.print(" x ");
+					} else if (item == -1) {
+						System.out.print(" b ");
+					}
+				}
+				System.out.println();
+				for (int i = 0; i < vetor.length; i++) {
+					System.out.print("[" + i + "]");
+				}
+				System.out.println();
+				System.out.println();
+				System.out.println("Parabéns, você ganhou o jogo!");
+				System.out.println("Pontuação: " + pontuacao + "/" + pontuacaoMaxima);
+				System.exit(0);
+			}
+
+			vetor[escolha] = 1;
 		}
-
-		// System.out.println("Antes");
-		/*
-		 * for (int i = 0; i < vetor.length; i++) {
-		 * 
-		 * if (bomba > 0) { vetor[i] = -1; bomba--; } else { vetor[i] = 0; }
-		 * //System.out.println(vetor[i]); }
-		 */
-
-		/*
-		 * // imprime o conteúdo do array (depois de embaralhado)
-		 * System.out.println("Depois de embaralhar: "); for (int i = 0; i <
-		 * vetor.length; i++) { System.out.println(vetor[i]); }
-		 */
 
 		entrada.close();
 
