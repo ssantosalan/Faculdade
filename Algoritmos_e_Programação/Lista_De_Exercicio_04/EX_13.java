f0d665623305cc6eadcdea0af6d18aa37a172f8c
Alan Santos <ss.alan@outlook.com>
2021-05-31 12:29:13 -0300

    Update EX_13.java

--- Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_13.java
+++ Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_13.java
@@ -2,8 +2,27 @@
 public class EX_13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int vetorA[] = {1, 2, 3};
+		int vetorB[] = {4, 5, 6};
+		int vetorC[] = new int[vetorA.length + vetorB.length];
+		int contador = 0;
+		for (int i = 0; i < vetorC.length; i++) {
+			
+			if (i < vetorA.length) {
+				vetorC[i] = vetorA[i]; 
+			} else {
+				vetorC[i] = vetorB[contador];
+				contador++;
+			}
+			
+			
+			
+		}
+		System.out.println("Vetor C:");
+		for (int item : vetorC)	{
+			System.out.println(item);
+		}
+  
 	}
 
 }
