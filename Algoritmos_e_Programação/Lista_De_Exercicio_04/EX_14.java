4ff7376f327f26dde76fdcdca58fce679c475905
Alan Santos <ss.alan@outlook.com>
2021-06-01 14:24:45 -0300

    Update EX_14.java

--- Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_14.java
+++ Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_14.java
@@ -2,27 +2,27 @@
 public class EX_14 {
 
 	public static void main(String[] args) {
-		int vetorA[] = {1, 2, 3};
-		int vetorB[] = {4, 5, 6};
+		int vetorA[] = { 1, 2, 3 };
+		int vetorB[] = { 10, 20, 30 };
 		int vetorC[] = new int[vetorA.length + vetorB.length];
-		int contador = 0;
+		int contadorA = 0;
+		int contadorB = 0;
 		for (int i = 0; i < vetorC.length; i++) {
-			
-			if (contador % 2 == 0) {
-				vetorC[i] = vetorA[i];
+
+			if (i % 2 == 0) {
+				vetorC[i] = vetorA[contadorA];
+				contadorA++;
 			} else {
-				vetorC[i] = vetorB[i-1];
+				vetorC[i] = vetorB[contadorB];
+				contadorB++;
 			}
-		
-			
-			
-			
+
 		}
 		System.out.println("Vetor C:");
-		for (int item : vetorC)	{
+		for (int item : vetorC) {
 			System.out.println(item);
 		}
-  
+
 	}
 
 }
