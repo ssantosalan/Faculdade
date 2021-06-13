203ec76efde3431a2823e884c1dec8d21ae74be2
Alan Santos <ss.alan@outlook.com>
2021-06-13 12:02:31 -0300

    Create EX_25.java

---
+++ Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_25.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class EX_25 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Linhas: ");
+		int n = entrada.nextInt();
+		
+		System.out.println("Colunas: ");
+		int m = entrada.nextInt();
+		
+		int matriz[][] = new int [n][m];
+		
+		for (int i = 0; i < matriz.length; i++) {
+			
+			for (int j = 0; j < matriz.length; j++) {
+				
+				matriz[i][j] = i;
+				
+			}
+			
+		}
+		
+		for (int[] vetor :  matriz) {
+			
+			for (int item : vetor) {
+				
+				System.out.print(item + "\t");
+				
+			}
+			System.out.println();
+			
+		}
+		
+		
+		entrada.close();
+
+	}
+
+}
