b1e0303a090e526a4edf593d4c472951049969d3
Alan Santos <ss.alan@outlook.com>
2021-06-16 15:27:11 -0300

    Create EX_26.java

---
+++ Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_26.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class EX_26 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Linhas: ");
+		int n = entrada.nextInt();
+		System.out.println("Colunas: ");
+		int m = entrada.nextInt();
+
+		int matriz[][] = new int[n][m];
+
+		for (int i = 0; i < matriz.length; i++) {
+
+			for (int j = 0; j < m; j++) {
+
+				matriz[i][j] = j;
+				
+			}
+
+		}
+		
+		for (int[] vetor : matriz) {
+			
+			for (int item : vetor) {
+				
+				System.out.print(item + "\t");
+				
+			}
+			System.out.println();
+		}
+		
+		
+
+		entrada.close();
+
+	}
+
+}
