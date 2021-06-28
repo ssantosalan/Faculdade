edfad486375d3136e33900fadd87693270217e34
Alan Santos <ss.alan@outlook.com>
2021-06-28 20:42:13 -0300

    Update EX_36.java

--- Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_36.java
+++ Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_36.java
@@ -12,6 +12,54 @@ public class EX_36 {
 		int m = entrada.nextInt();
 
 		int matriz[][] = new int[n][m];
+
+		for (int i = 0; i < matriz.length; i++) {
+
+			for (int j = 0; j < m; j++) {
+
+				System.out.println("Elemento de posição " + i + j);
+				matriz[i][j] = entrada.nextInt();
+			}
+
+		}
+		System.out.println();
+		
+		for (int vetor[] : matriz) {
+
+			for (int item : vetor) {
+				System.out.print(item + "\t");
+
+			}
+			System.out.println();
+
+		}
+		System.out.println();
+
+		
+		if (n == m) { //P poder pegar a diagonal principal
+			
+			for (int i = 0; i < matriz.length; i++) {
+
+				for (int j = 0; j < m; j++) {
+
+					if (i == j) {
+						System.out.print(matriz[i][i] + "\t");
+					} else {
+						System.out.print("x" + "\t");
+					}
+					
+
+				}
+				System.out.println();
+
+			}
+			
+			
+		} else {
+			
+			System.out.println("Não possui diagonal principal.");
+			
+		}
 		
 		
 
