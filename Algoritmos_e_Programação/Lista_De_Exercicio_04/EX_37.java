27cb00fc32ff49070c44a4be023a9305dd767e42
Alan Santos <ss.alan@outlook.com>
2021-06-29 18:59:58 -0300

    Update EX_37.java

--- Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_37.java
+++ Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_37.java
@@ -1,8 +1,68 @@
+import java.util.Scanner;
 
 public class EX_37 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Linhas: ");
+		int n = entrada.nextInt();
+		System.out.println("Colunas: ");
+		int m = entrada.nextInt();
+
+		int matriz[][] = new int[n][m];
+
+		for (int i = 0; i < matriz.length; i++) {
+
+			for (int j = 0; j < m; j++) {
+
+				System.out.println("Elemento de posição " + i + j);
+				matriz[i][j] = entrada.nextInt();
+
+			}
+
+		}
+
+		System.out.println();
+
+		for (int vetor[] : matriz) {
+
+			for (int item : vetor) {
+
+				System.out.print(item + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		int somaColuna = 0;
+		int contadorLinha = 0;
+
+		for (int i = 0; i < matriz.length; i++) {
+			
+			int somaLinha = 0;
+			
+			for (int j = 0; j < m; j++) {
+
+				somaLinha = somaLinha + matriz[i][j];
+				
+						
+			}
+			
+			if (somaLinha == 0) {
+				contadorLinha++;
+			}
+			
+			
+			
+		}
+		
+		System.out.println("Número de linhas nulas: " + contadorLinha);
+		
+		
+				
+		
+		entrada.close();
 
 	}
 
