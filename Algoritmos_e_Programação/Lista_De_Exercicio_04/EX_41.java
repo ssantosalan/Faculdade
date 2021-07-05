d71ba9079078273dcfb4b2e74987949752b3af0d
Alan Santos <ss.alan@outlook.com>
2021-07-05 17:27:25 -0300

    Create EX_41.java

---
+++ Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_41.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EX_41 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Linhas: ");
+		int n = entrada.nextInt();
+
+		System.out.println("Colunas: ");
+		int m = entrada.nextInt();
+
+		int matriz[][] = new int[n][m];
+
+		entrada.close();
+
+	}
+
+}
