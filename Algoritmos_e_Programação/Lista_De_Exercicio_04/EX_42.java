5c06599589901ec6a7b8e90c3da896b7fc6f3fac
Alan Santos <ss.alan@outlook.com>
2021-08-08 20:20:57 -0300

    Update EX_42.java

--- Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_42.java
+++ Algoritmos_e_Programação/Lista_De_Exercicio_04/EX_42.java
@@ -10,7 +10,7 @@ public class EX_42 {
 
 		System.out.println("Colunas: ");
 		int m = entrada.nextInt();
-
+//second sem
 		int matrizA[][] = new int[n][m];
 		int matrizB[][] = new int[n][m];
 		int matrizC[][] = new int[n][m];
@@ -27,6 +27,7 @@ public class EX_42 {
 		int resultado;
 		int zero;
 		int finish;
+		
 		
 		
 		entrada.close();
