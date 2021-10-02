4952e67c17aa58e5927a0e6df8cba96392021495
Alan Santos <alanxdsi@gmail.com>
2021-10-02 12:28:13 -0300

    Create ComparacaoCompareTo.java

---
+++ Algoritmos_II/Teste/src/ComparacaoCompareTo.java
@@ -0,0 +1,16 @@
+
+public class ComparacaoCompareTo {
+
+	public static void main(String[] args) {
+		System.out.println("Testando");
+		String nome1 = "Bia";
+		String nome2 = "Alansahuisahuisuahi";
+		
+		String nome3 = "Alan";
+		String nome4 = "Alen";
+
+		System.out.println(nome1.compareTo(nome2));
+		System.out.println(nome3.compareTo(nome4));
+	}
+
+}
