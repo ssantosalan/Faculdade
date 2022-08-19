24ea1df77f6a897240ff6280266e5bec7d5b0182
ALAN SILVA DOS SANTOS <alan.ssantos14@senacsp.edu.br>
2022-08-18 22:21:23 -0300

    commit

--- QuartoSemestre/Arquitetura de Software/Aula01/Aula02/Singleton.java
+++ QuartoSemestre/Arquitetura de Software/Aula01/Aula02/Singleton.java
@@ -4,9 +4,13 @@ public class Singleton {
 
     private static Singleton singleton = new Singleton();
 
-    private Singleton(){}
+    private Singleton() {
+    }
 
     public static Singleton getIntancia() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
         return singleton;
     }
 
--- QuartoSemestre/Arquitetura de Software/Aula01/Aula02/UsaSingleton.java
+++ QuartoSemestre/Arquitetura de Software/Aula01/Aula02/UsaSingleton.java
@@ -2,6 +2,9 @@ package Aula02;
 
 public class UsaSingleton {
     public static void main(String[] args) {
-
+        Singleton sin1 = Singleton.getIntancia();
+        Singleton sin2 = Singleton.getIntancia();
+        System.out.println(sin1);
+        System.out.println(sin2);
     }
 }
